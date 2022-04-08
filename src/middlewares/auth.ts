644f2a9346d9878473561822adb5a5c68eaa1a8c
mathieutercan <mathieu.tercan@epitech.eu>
2022-04-08 13:32:16 +0200

    [Add] Middlewares ans app.js

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,48 @@
+import jwt from 'express-jwt';
+import UserModel from '../models/UserModel';
+import apiResponse from '../helpers/apiResponse';
+import { errorMessages } from '../helpers/constants';
+import type express from "express";
+const dotenv = require('dotenv');
+dotenv.config();
+
+const secret = process.env.JWT_SECRET;
+
+// eslint-disable-next-line consistent-return,max-len
+async function checkUserExists(req: express.Request, res: express.Response, next: express.NextFunction) {
+    try {
+        // eslint-disable-next-line no-underscore-dangle
+        const user = await UserModel.findOne({ _id: req['user']._id });
+        if (!user) {
+            return apiResponse.unauthorizedResponse(res, errorMessages.userNoExist);
+        }
+        if (user.isBan()) {
+            return apiResponse.unauthorizedResponse(res, errorMessages.bannedUser);
+        }
+        req['user'] = user;
+
+        return next();
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+// eslint-disable-next-line max-len,consistent-return
+async function checkValidationEmail(req: express.Request, res: express.Response, next: express.NextFunction) {
+    try {
+        if (!req['user'].verifiedEmail) {
+            return apiResponse.unauthorizedResponse(res, errorMessages.emailNotVerified);
+        }
+        return next();
+    } catch (e) {
+        console.log(e);
+    }
+}
+exports.checkUser = [
+    jwt({ secret, algorithms: ['HS256'] }),
+    checkUserExists,
+];
+
+exports.checkValidationEmail = [
+    checkValidationEmail,
+];
