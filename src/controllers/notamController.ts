e157f2ecf76959231e1e6c45c55de00137578c9a
Mathieu Tercan <mathieu@futures-factory.com>
2022-09-13 13:42:33 +0200

    [merge]

---
+++ src/controllers/notamController.ts
@@ -0,0 +1,29 @@
+import fetch from 'node-fetch';
+import express from 'express';
+
+const authMiddlewares = require('../middlewares/auth');
+const responseApi = require('../helpers/apiResponse');
+const { errors } = require('../helpers/constants');
+
+const Authorization = process.env.TOKEN_METAR_TAF;
+
+async function get(req: express.Request, res: express.Response) {
+    try {
+        if (!req.body.airport) {
+            // eslint-disable-next-line max-len
+            return responseApi.errorResponse(res, errors.missArgument.code, errors.missArgument.message);
+        }
+        const response = await fetch(`https://avwx.rest/api/metar/${req.body.airport}?airport=true&format=json&onfail=cache`, {
+            headers: { Authorization },
+        });
+        return responseApi.successResponseWithData(res, await response.json());
+    } catch (e) {
+        // eslint-disable-next-line max-len
+        return responseApi.errorResponse(res, errors.interneError.code, errors.interneError.message);
+    }
+}
+
+exports.getNotam = [
+    authMiddlewares.checkUser,
+    get,
+];
---
+++ src/routes/tests/pro.js
@@ -0,0 +1,86 @@
+/* eslint-disable no-undef */
+// const mongoose = require('mongoose');
+const supertest = require('supertest');
+
+const app = require('../../app');
+
+const database = 'mongodb://mongoDB:27017/profile-test';
+
+describe('Profile tests', () => {
+    beforeAll(async () => {
+        try {
+            await mongoose.connect(database, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            });
+        } catch (e) {
+            console.error(e)
+            process.exit(1)
+        }
+    })
+
+    afterAll(async () => {
+        const collections = await mongoose.connection.db.collections()
+
+        for (let collection of collections) {
+            await collection.deleteMany()
+        }
+
+        await mongoose.disconnect()
+    })
+    // try {
+    //     await mongoose.connect(database, {
+    //         useNewUrlParser: true,
+    //         useUnifiedTopology: true
+    //     });
+    // } catch (e) {
+    //     console.error(e)
+    //     process.exit(1)
+    // }
+
+    afterAll(async () => {
+        const collections = await mongoose.connection.db.collections()
+
+        for (let collection of collections) {
+            await collection.deleteMany()
+        }
+
+        await mongoose.disconnect()
+    });
+
+    const user = {
+        email: 'test@test.test',
+        password: 'password',
+    };
+
+    let jwt = null;
+    it('should register an user', async () => {
+        const resRegister = await supertest(app)
+            .post('/api/auth/register')
+            .send(user);
+
+        jwt = resRegister.body.jwtToken;
+    });
+
+    describe('Get profile', () => {
+        it('should return unauthorized error', async () => {
+            const response = await supertest(app)
+                .get('/api/profile')
+                .set('Authorization', `Bearer ${jwt}1`);
+
+            expect(response.status)
+                .toBe(401);
+        });
+
+        it.skip('should return the profile', async () => {
+            const response = await supertest(app)
+                .get('/api/profile')
+                .set('Authorization', `Bearer ${jwt}`);
+
+            expect(response.status)
+                .toBe(200);
+            expect(response.body.email)
+                .toBe(user.email);
+        });
+    })
+})
